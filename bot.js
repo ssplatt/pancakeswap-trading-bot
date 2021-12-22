16c7b4247333f190d0b450386aeb9d0e49f90b2a
Brett Taylor <sweet.brett@gmail.com>
2021-12-22 09:33:10 -0500

    working buys (#1)

--- bot.js
+++ bot.js
@@ -1,190 +1,141 @@
-import ethers from 'ethers';
-import express from 'express';
-import chalk from 'chalk';
-import dotenv from 'dotenv';
-import inquirer from 'inquirer';
+import ethers from 'ethers'
+import chalk from 'chalk'
+import dotenv from 'dotenv'
 
-const app = express();
-dotenv.config();
+dotenv.config()
 
-const data = {
-  BNB: process.env.BNB_CONTRACT, //bnb
-
-  to_PURCHASE: process.env.TO_PURCHASE, // token that you will purchase = BUSD for test '0xe9e7cea3dedca5984780bafc599bd69add087d56'
-
-  AMOUNT_OF_BNB : process.env.AMOUNT_OF_BNB, // how much you want to buy in BNB
-
-  factory: process.env.FACTORY,  //PancakeSwap V2 factory
-
-  router: process.env.ROUTER, //PancakeSwap V2 router
-
-  recipient: process.env.YOUR_ADDRESS, //your wallet address,
-
-  Slippage : process.env.SLIPPAGE, //in Percentage
-
-  gasPrice : ethers.utils.parseUnits(`${process.env.GWEI}`, 'gwei'), //in gwei
-
-  gasLimit : process.env.GAS_LIMIT, //at least 21000
-
-  minBnb : process.env.MIN_LIQUIDITY_ADDED //min liquidity added
+const config = {
+  bnb: process.env.BNB_CONTRACT,
+  toPurchase: process.env.TO_PURCHASE,
+  amountOfBnb: process.env.AMOUNT_OF_BNB,
+  factory: process.env.FACTORY,
+  router: process.env.ROUTER,
+  recipient: process.env.YOUR_ADDRESS,
+  slippage : process.env.SLIPPAGE,
+  gasPrice : ethers.utils.parseUnits(`${process.env.GWEI}`, 'gwei'),
+  gasLimit : process.env.GAS_LIMIT,
+  minBnb : process.env.MIN_LIQUIDITY_ADDED
 }
 
-let initialLiquidityDetected = false;
-let jmlBnb = 0;
+let initialLiquidityDetected = false
+let jmlBnb = 0
 
-const wss = process.env.WSS_NODE;
-const mnemonic = process.env.YOUR_MNEMONIC //your memonic;
-const tokenIn = data.BNB;
-const tokenOut = data.to_PURCHASE;
-// const provider = new ethers.providers.JsonRpcProvider(bscMainnetUrl)
-const provider = new ethers.providers.WebSocketProvider(wss);
-const wallet = new ethers.Wallet(mnemonic);
-const account = wallet.connect(provider);
-
+const wss = process.env.WSS_NODE
+const mnemonic = process.env.YOUR_MNEMONIC
+const tokenIn = config.bnb
+const tokenOut = config.toPurchase
+const provider = new ethers.providers.WebSocketProvider(wss)
+const wallet = new ethers.Wallet.fromMnemonic(mnemonic)
+const account = wallet.connect(provider)
 
 const factory = new ethers.Contract(
-  data.factory,
-  [
-    'event PairCreated(address indexed token0, address indexed token1, address pair, uint)',
-    'function getPair(address tokenA, address tokenB) external view returns (address pair)'
-  ],
-  account
+    config.factory,
+    [
+        'event PairCreated(address indexed token0, address indexed token1, address pair, uint)',
+        'function getPair(address tokenA, address tokenB) external view returns (address pair)'
+    ],
+    account
 );
-
+  
 const router = new ethers.Contract(
-  data.router,
-  [
-    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
-    'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
-    'function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline) external  payable returns (uint[] memory amounts)',
-    'function swapExactETHForTokens( uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)',
-  ],
-  account
+    config.router,
+    [
+        'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+        'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
+        'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
+        'function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline) external  payable returns (uint[] memory amounts)',
+        'function swapExactETHForTokens( uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)',
+    ],
+    account
 );
-
+  
 const erc = new ethers.Contract(
-  data.BNB,
-  [{"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"type": "function"}],
-  account
+    config.bnb,
+    [{"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"type": "function"}],
+    account
 );
 
 const run = async () => {
-    await checkLiq();
+    await checkLiq()
 }
 
-  let checkLiq = async() => {
-    const pairAddressx = await factory.getPair(tokenIn, tokenOut);
-    console.log(chalk.blue(`pairAddress: ${pairAddressx}`));
+let checkLiq = async() => {
+    const pairAddressx = await factory.getPair(tokenIn, tokenOut)
+    console.log(chalk.blue(`pairAddress: ${pairAddressx}`))
     if (pairAddressx !== null && pairAddressx !== undefined) {
-      // console.log("pairAddress.toString().indexOf('0x0000000000000')", pairAddress.toString().indexOf('0x0000000000000'));
-      if (pairAddressx.toString().indexOf('0x0000000000000') > -1) {
-        console.log(chalk.cyan(`pairAddress ${pairAddressx} not detected. Auto restart`));
-        return await run();
-      }
+        if (pairAddressx.toString().indexOf('0x0000000000000') > -1) {
+            console.log(chalk.cyan(`pairAddress ${pairAddressx} not detected. Auto restart`))
+            return await run()
+        }
     }
-    const pairBNBvalue = await erc.balanceOf(pairAddressx);
-    jmlBnb = await ethers.utils.formatEther(pairBNBvalue);
-    console.log(`value BNB : ${jmlBnb}`);
+    const pairBnbValue = await erc.balanceOf(pairAddressx)
+    jmlBnb = ethers.utils.formatEther(pairBnbValue)
+    console.log('value BNB:', jmlBnb)
 
-    if(parseFloat(jmlBnb) > parseFloat(data.minBnb)){
-        setTimeout(() => buyAction(), 3000);
+    if(parseFloat(jmlBnb) > parseFloat(config.minBnb)){
+        console.log('initiating buy...')
+        setTimeout(() => buyAction(), 3000)
     }
     else{
-        initialLiquidityDetected = false;
-        console.log(' run again...');
-        return await run();
-      }
-  }
+        initialLiquidityDetected = false
+        console.log('run again...')
+        return await run()
+    }
+}
 
-  let buyAction = async() => {
+let buyAction = async() => {
     if(initialLiquidityDetected === true) {
-      console.log('not buy cause already buy');
-        return null;
+        console.log('already bought')
+        return null
     }
 
-    console.log('ready to buy');
-    try{
-      initialLiquidityDetected = true;
+    console.log('ready to buy')
+    try {
+        initialLiquidityDetected = true
 
-      let amountOutMin = 0;
-      //We buy x amount of the new token for our bnb
-      const amountIn = ethers.utils.parseUnits(`${data.AMOUNT_OF_BNB}`, 'ether');
-      if ( parseInt(data.Slippage) !== 0 ){
-        const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
-        //Our execution price will be a bit different, we need some flexibility
-        amountOutMin = amounts[1].sub(amounts[1].div(`${data.Slippage}`))
-      }
+        let amountOutMin = 0
+        const amountIn = ethers.utils.parseUnits(`${config.amountOfBnb}`, 'ether')
+        if ( parseInt(config.slippage) !== 0 ){
+            const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut])
+            amountOutMin = amounts[1].sub(amounts[1].div(`${config.slippage}`))
+        }
 
-      console.log(
-       chalk.green.inverse(`Start to buy \n`)
+        console.log(
+        chalk.green.inverse('Start to buy \n')
         +
         `Buying Token
         =================
         tokenIn: ${(amountIn * 1e-18).toString()} ${tokenIn} (BNB)
         tokenOut: ${amountOutMin.toString()} ${tokenOut}
-      `);
+        `);
 
-      console.log('Processing Transaction.....');
-      console.log(chalk.yellow(`amountIn: ${(amountIn * 1e-18)} ${tokenIn} (BNB)`));
-      console.log(chalk.yellow(`amountOutMin: ${amountOutMin}`));
-      console.log(chalk.yellow(`tokenIn: ${tokenIn}`));
-      console.log(chalk.yellow(`tokenOut: ${tokenOut}`));
-      console.log(chalk.yellow(`data.recipient: ${data.recipient}`));
-      console.log(chalk.yellow(`data.gasLimit: ${data.gasLimit}`));
-      console.log(chalk.yellow(`data.gasPrice: ${data.gasPrice}`));
+        console.log('Processing Transaction.....')
+        console.log(chalk.yellow(`amountIn: ${(amountIn * 1e-18)} ${tokenIn} (BNB)`))
+        console.log(chalk.yellow(`amountOutMin: ${amountOutMin}`))
+        console.log(chalk.yellow(`tokenIn: ${tokenIn}`))
+        console.log(chalk.yellow(`tokenOut: ${tokenOut}`))
+        console.log(chalk.yellow(`config.recipient: ${config.recipient}`))
+        console.log(chalk.yellow(`config.gasLimit: ${config.gasLimit}`))
+        console.log(chalk.yellow(`config.gasPrice: ${config.gasPrice}`))
 
-      // const tx = await router.swapExactTokensForTokensSupportingFeeOnTransferTokens( //uncomment this if you want to buy deflationary token
-      const tx = await router.swapExactETHForTokens( //uncomment here if you want to buy token
-        amountOutMin,
-        [tokenIn, tokenOut],
-        data.recipient,
-        Date.now() + 1000 * 60 * 5, //5 minutes
-        {
-          'gasLimit': data.gasLimit,
-          'gasPrice': data.gasPrice,
-          'nonce' : null, //set you want buy at where position in blocks
-          'value' : amountIn
-      });
+        const tx = await router.swapExactETHForTokens(
+            amountOutMin,
+            [tokenIn, tokenOut],
+            config.recipient,
+            Date.now() + 1000 * 60 * 5, //5 minutes
+            {
+                'gasLimit': config.gasLimit,
+                'gasPrice': config.gasPrice,
+                'nonce' : null,
+                'value' : amountIn
+            })
 
-      const receipt = await tx.wait();
-      console.log(`Transaction receipt : https://www.bscscan.com/tx/${receipt.logs[1].transactionHash}`);
-      setTimeout(() => {process.exit()},2000);
-    }catch(err){
-      let error = JSON.parse(JSON.stringify(err));
-        console.log(`Error caused by : 
-        {
-        reason : ${error.reason},
-        transactionHash : ${error.transactionHash}
-        message : ${error}
-        }`);
-        console.log(error);
-
-        inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'runAgain',
-      message: 'Do you want to run again thi bot?',
-    },
-  ])
-  .then(answers => {
-    if(answers.runAgain === true){
-      console.log('= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =');
-      console.log('Run again');
-      console.log('= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =');
-      initialLiquidityDetected = false;
-      run();
-    }else{
-      process.exit();
+        const receipt = await tx.wait()
+        console.log(`Transaction receipt : https://www.bscscan.com/tx/${receipt.logs[1].transactionHash}`)
+        setTimeout(() => {process.exit()},2000)
+    } catch(err) {
+        console.log(err)
     }
+}
 
-  });
-
-    }
-  }
-
-run();
-
-const PORT = 5001;
-
-app.listen(PORT, console.log(chalk.yellow(`Listening for Liquidity Addition to token ${data.to_PURCHASE}`)));
+run()
